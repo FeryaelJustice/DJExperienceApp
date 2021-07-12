e583cdd7be594160a024a1ccc144ecd61ae7f55b
DESKTOP-519O059\nano9 <nano0gs@hotmail.es>
2021-07-12 02:22:44 +0200

    OnBoarding first step

--- app/src/main/java/com/feryaeldev/djexperience/activities/MainActivity.kt
+++ app/src/main/java/com/feryaeldev/djexperience/activities/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.feryaeldev.djexperience.activities
 
+import android.content.Intent
 import android.os.Bundle
 import com.feryaeldev.djexperience.R
 import com.feryaeldev.djexperience.base.BaseActivity
+import com.feryaeldev.djexperience.onboarding.OnboardingActivity
 
 class MainActivity : BaseActivity() {
+
+    private var goToOnBoardingActivity = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (goToOnBoardingActivity) {
+            navigateToOnboarding()
+        }
+    }
+
+    private fun navigateToOnboarding() {
+        startActivity(Intent(applicationContext, OnboardingActivity::class.java))
+        finish()
     }
 }
---
+++ app/src/main/java/com/feryaeldev/djexperience/onboarding/OnboardingActivity.kt
@@ -0,0 +1,12 @@
+package com.feryaeldev.djexperience.onboarding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.feryaeldev.djexperience.R
+
+class OnboardingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding)
+    }
+}
