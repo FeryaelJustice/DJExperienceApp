66aa05fef50b634095c1a0a0b97a749c27dc4a40
DESKTOP-519O059\nano9 <nano0gs@hotmail.es>
2021-07-16 01:32:39 +0200

    wipe unnecesary stuff

--- app/src/main/java/com/feryaeldev/djexperience/activities/MainActivity.kt
+++ app/src/main/java/com/feryaeldev/djexperience/activities/MainActivity.kt
@@ -2,55 +2,71 @@ package com.feryaeldev.djexperience.activities
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TextView
-import androidx.activity.viewModels
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.feryaeldev.djexperience.R
 import com.feryaeldev.djexperience.base.BaseActivity
-import com.feryaeldev.djexperience.data.enums.ArtistType
-import com.feryaeldev.djexperience.data.models.Artist
-import com.feryaeldev.djexperience.data.models.User
-import com.feryaeldev.djexperience.data.viewmodels.ArtistViewModel
-import com.feryaeldev.djexperience.data.viewmodels.UserViewModel
 import com.feryaeldev.djexperience.onboarding.OnboardingActivity
 import com.feryaeldev.djexperience.settings.Settings
-import com.google.android.gms.ads.*
+import com.google.android.gms.ads.AdView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : BaseActivity() {
 
-    lateinit var mAdView: AdView
-    private val userViewModel: UserViewModel by viewModels()
-    private val artistViewModel: ArtistViewModel by viewModels()
+    //lateinit var mAdView: AdView
+    //private val userViewModel: UserViewModel by viewModels()
+    //private val artistViewModel: ArtistViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // VIEW MODELS
-        userViewModel.getUser().observe(this, {
-            findViewById<TextView>(R.id.tv1).text = it.edad.toString()
-        })
-        artistViewModel.getArtist().observe(this, {
-            findViewById<TextView>(R.id.tv2).text = it.edad.toString()
-        })
-
         // SETTINGS
+
         //val preferences = getSharedPreferences("Settings", Context.MODE_PRIVATE) ?: return
         val settings = Settings(applicationContext)
         if (settings.isFirstOpen()) {
             navigateToOnboarding()
         }
 
-        // CRASHLYTICS
-        val crashButton = Button(this)
-        crashButton.text = "Crash!"
-        crashButton.setOnClickListener {
-            throw RuntimeException("Test Crash") // Force a crash
+        // FRAGMENTS BOTTOM NAV AND NAVIGATION COMPONENT
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNav)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        bottomNavigationView.setupWithNavController(navController)
+        //NavigationUI.setupWithNavController(bottomNavigationView, navController)
+        /*
+        bottomNavigationView.selectedItemId = R.id.action_home
+        bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.action_home -> {
+                    replaceFragment(HomeFragment.newInstance())
+                    true
+                }
+                R.id.action_search -> {
+                    replaceFragment(SearchFragment.newInstance())
+                    true
+                }
+                R.id.action_profile -> {
+                    replaceFragment(ProfileFragment.newInstance())
+                    true
+                }
+                else -> false
+            }
         }
+        */
+
+        // VIEW MODELS
+
+        /*
+        userViewModel.getUser().observe(this, {
+            findViewById<TextView>(R.id.tv1).text = it.edad.toString()
+        })
+        artistViewModel.getArtist().observe(this, {
+            findViewById<TextView>(R.id.tv2).text = it.edad.toString()
+        })
         findViewById<Button>(R.id.id).setOnClickListener {
             userViewModel.setUser(
                 User(
@@ -67,16 +83,11 @@ class MainActivity : BaseActivity() {
                 )
             )
         }
-
-        addContentView(
-            crashButton,
-            ViewGroup.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.WRAP_CONTENT
-            ),
-        )
+        */
 
         // ADS
+
+        /*
         MobileAds.initialize(this) {}
 
         mAdView = findViewById(R.id.adView)
@@ -105,35 +116,23 @@ class MainActivity : BaseActivity() {
                 // to the app after tapping on an ad.
             }
         }
-
-        // Fragments
-
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNav)
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
-        bottomNavigationView.setupWithNavController(navController)
-        //NavigationUI.setupWithNavController(bottomNavigationView, navController)
-        /*
-        bottomNavigationView.selectedItemId = R.id.action_home
-        bottomNavigationView.setOnItemSelectedListener {
-            when (it.itemId) {
-                R.id.action_home -> {
-                    replaceFragment(HomeFragment.newInstance())
-                    true
-                }
-                R.id.action_search -> {
-                    replaceFragment(SearchFragment.newInstance())
-                    true
-                }
-                R.id.action_profile -> {
-                    replaceFragment(ProfileFragment.newInstance())
-                    true
-                }
-                else -> false
-            }
-        }
         */
+
+        // CRASHLYTICS
+
+        /*val crashButton = Button(this)
+        crashButton.text = "Crash!"
+        crashButton.setOnClickListener {
+            throw RuntimeException("Test Crash") // Force a crash
+        }
+
+        addContentView(
+            crashButton,
+            ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            ),
+        )*/
     }
 
     private fun navigateToOnboarding() {
