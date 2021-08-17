2eeca9c4b358df364c088d4d93a9670cc06ebf23
Feryael Justice <nano9gs@hotmail.es>
2021-08-17 02:08:41 +0200

    Home structure

---
+++ app/src/main/java/com/feryaeldev/djexperience/data/models/New.kt
@@ -0,0 +1,3 @@
+package com.feryaeldev.djexperience.data.models
+
+data class New(val id: String)
--- app/src/main/java/com/feryaeldev/djexperience/fragments/home/HomeFragment.kt
+++ app/src/main/java/com/feryaeldev/djexperience/fragments/home/HomeFragment.kt
@@ -4,15 +4,33 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.feryaeldev.djexperience.R
 import com.feryaeldev.djexperience.base.BaseFragment
+import com.feryaeldev.djexperience.common.NewsRecyclerViewAdapter
+import com.feryaeldev.djexperience.data.models.New
 
 class HomeFragment : BaseFragment() {
+
+    lateinit var mRecyclerView: RecyclerView
+    lateinit var mAdapter: NewsRecyclerViewAdapter
+
+    private val newsList: MutableList<New> = arrayListOf()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+
+        mRecyclerView = view.findViewById(R.id.news_rv)
+        mRecyclerView.layoutManager = LinearLayoutManager(view.context)
+
+        mAdapter = NewsRecyclerViewAdapter(newsList)
+
+        mRecyclerView.adapter = mAdapter
+
+        return view
     }
 }
