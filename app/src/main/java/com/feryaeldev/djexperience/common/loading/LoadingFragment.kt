e3530cf24fcb401d48a393b3abe257a75dc88cd4
Feryael Justice <nano9gs@hotmail.es>
2021-08-17 02:36:38 +0200

    Loading implemented

---
+++ app/src/main/java/com/feryaeldev/djexperience/common/loading/LoadingFragment.kt
@@ -0,0 +1,19 @@
+package com.feryaeldev.djexperience.common.loading
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.feryaeldev.djexperience.R
+import com.feryaeldev.djexperience.base.BaseFragment
+
+class LoadingFragment : BaseFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_loading, container, false)
+    }
+
+}
--- app/src/main/java/com/feryaeldev/djexperience/fragments/home/HomeFragment.kt
+++ app/src/main/java/com/feryaeldev/djexperience/fragments/home/HomeFragment.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentContainerView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.feryaeldev.djexperience.R
 import com.feryaeldev.djexperience.base.BaseFragment
 import com.feryaeldev.djexperience.common.NewsRecyclerViewAdapter
 import com.feryaeldev.djexperience.data.models.New
+import kotlinx.coroutines.*
 
 class HomeFragment : BaseFragment() {
 
@@ -18,11 +20,14 @@ class HomeFragment : BaseFragment() {
 
     private val newsList: MutableList<New> = arrayListOf()
 
+    @DelicateCoroutinesApi
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_home, container, false)
+
+        val progressCircle = view.findViewById<FragmentContainerView>(R.id.fragmentProgressBar)
 
         mRecyclerView = view.findViewById(R.id.news_rv)
         mRecyclerView.layoutManager = LinearLayoutManager(view.context)
@@ -31,6 +36,19 @@ class HomeFragment : BaseFragment() {
 
         mRecyclerView.adapter = mAdapter
 
+        GlobalScope.launch(Dispatchers.Main) {
+            val res = callServer()
+            progressCircle.visibility = View.GONE
+            mRecyclerView.visibility = View.VISIBLE
+        }
+
         return view
+    }
+
+    private suspend fun callServer() {
+        return withContext(Dispatchers.IO) {
+            delay(2000)
+
+        }
     }
 }
