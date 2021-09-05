75ab374a51468442ade12d12a970be8253889a16
Feryael Justice <nano9gs@hotmail.es>
2021-09-05 23:39:38 +0200

    Create Artist 100%

--- app/src/main/java/com/feryaeldev/djexperience/ui/view/fragments/create/artist/CreateArtistFragment.kt
+++ app/src/main/java/com/feryaeldev/djexperience/ui/view/fragments/create/artist/CreateArtistFragment.kt
@@ -1,17 +1,30 @@
 package com.feryaeldev.djexperience.ui.view.fragments.create.artist
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.navigation.fragment.findNavController
 import com.feryaeldev.djexperience.R
+import com.feryaeldev.djexperience.data.model.domain.User
 import com.feryaeldev.djexperience.ui.base.BaseFragment
+import com.feryaeldev.djexperience.util.asMap
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
+import com.squareup.picasso.Picasso
 
 class CreateArtistFragment : BaseFragment() {
+
+    private lateinit var uri: Uri
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,8 +32,77 @@ class CreateArtistFragment : BaseFragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_create_artist, container, false)
 
+        val image: ImageView = view.findViewById(R.id.createArtist_photo)
+        val email: EditText = view.findViewById(R.id.createArtist_email)
+        val username: EditText = view.findViewById(R.id.createArtist_username)
+        val name: EditText = view.findViewById(R.id.createArtist_name)
+        val surnames: EditText = view.findViewById(R.id.createArtist_surnames)
+        val country: EditText = view.findViewById(R.id.createArtist_country)
+        val age: EditText = view.findViewById(R.id.createArtist_age)
+        val website: EditText = view.findViewById(R.id.createArtist_website)
+
+        // Profile picture
+        val resultLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == Activity.RESULT_OK) {
+                    result.data?.data?.let { url ->
+                        uri = url
+                        Picasso.get().load(url).into(image)
+                    }
+                }
+            }
+        image.setOnClickListener {
+            val intentPick = Intent(Intent.ACTION_PICK)
+            intentPick.type = "image/*"
+            resultLauncher.launch(intentPick)
+        }
+
+        val db = Firebase.firestore
+        val artistsDocRef = db.collection("artists")
+        val profilePicsRef =
+            Firebase.storage.reference
+
         view.findViewById<Button>(R.id.createArtist_saveBtn).setOnClickListener {
-            Toast.makeText(view.context,"Save function very soon...",Toast.LENGTH_SHORT).show();
+            if (username.text.toString().isNotBlank() && email.text.toString()
+                    .isNotBlank() && name.text.toString()
+                    .isNotBlank() && surnames.text.toString()
+                    .isNotBlank() && country.text.toString().isNotBlank() && age.text.toString()
+                    .isNotBlank() && website.text.toString().isNotBlank()
+            ) {
+                val user = User()
+                user.id = randomString(28)
+                user.email = email.text.toString()
+                user.username = username.text.toString()
+                user.name = name.text.toString()
+                user.surnames = surnames.text.toString()
+                user.country = country.text.toString()
+                user.category = "Artist"
+                user.age = age.text.toString().toLongOrNull()
+                user.website = website.text.toString()
+
+                if (uri != null) {
+                    artistsDocRef.document(user.id!!).set(user.asMap())
+                        .addOnSuccessListener {
+                            profilePicsRef.child("profile_images/${user.id}.jpg").putFile(uri)
+                                .addOnCompleteListener {
+                                    if (it.isSuccessful) {
+                                        showMessageLong("Artist created!")
+                                        findNavController().popBackStack()
+                                    } else {
+                                        showMessageLong("Failed! Error on uploading image...")
+                                    }
+                                }
+                        }.addOnFailureListener {
+                            showMessageShort("Failed!")
+                        }
+                }
+            } else {
+                Toast.makeText(
+                    view.context,
+                    "At least one of the fields is empty!",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
         }
 
         // Back
@@ -29,5 +111,10 @@ class CreateArtistFragment : BaseFragment() {
         }
 
         return view
+    }
+
+    fun randomString(length: Int): String {
+        val allowedChars = ('A'..'Z') + ('a'..'z') + ('0'..'9')
+        return (1..length).map { allowedChars.random() }.joinToString("")
     }
 }
